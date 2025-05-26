1cee627111d0bdfb7d31eb1fffbe4fbd00a53429
Pablo Orviz <orviz@ifca.unican.es>
2025-05-26 15:15:10 +0200

    Add JePL file structure

---
+++ .sqa/script.messy-beige-jackal.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.geo3bcn.csic.es/dtgeo_geo3bcn/dtc-v2/trigger &&
+    mdl --json  .
+)
